6fa3e961e9a1b4fe626c3d59b032d6ee4fbae0e5
Boukedir Mohamed <bouukedir@outlook.com>
2022-02-25 15:44:07 +0100

    2 commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,8 @@
 public class HelloWorld {
-
 	public static void main(String[] args) {
-		System.out.println("Hello World  !!");
-			
+		System.out.println("Hello World 222222 !!!!!!");
 	}
-
 }
 
 
-//System.out.println("Hello World 2 !!!!!!");
+
